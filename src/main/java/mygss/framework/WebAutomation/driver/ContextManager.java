6b0fad5b1262fec89423fad0eaa4def32fd5398b
QTP <QTP@192.168.1.7>
2018-12-17 20:47:57 +0530

    XMLHelper

---
+++ src/main/java/mygss/framework/WebAutomation/driver/ContextManager.java
@@ -0,0 +1,50 @@
+package mygss.framework.WebAutomation.driver;
+
+import java.io.File;
+import java.util.Map;
+
+import org.testng.ITestContext;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class ContextManager {
+	
+	private static Context globalContext;
+	
+	private static ThreadLocal<Context> localContext = new ThreadLocal<Context>();
+	
+	public static Context getLocalContext() {
+		return localContext.get();
+	}
+
+	public static Context getGlobalContext() {
+		return globalContext;
+	}
+
+	public static void initThreadConytext(ITestContext testNGContext) {
+		Context ctx = new Context(testNGContext);
+		localContext.set(ctx);
+	}
+	
+	public static void initGlobalContext(ITestContext iTestCtx) {
+		iTestCtx = getContextFromConfigFile(iTestCtx);
+	}
+
+	private static ITestContext getContextFromConfigFile(ITestContext iTestCtx) {
+		if(iTestCtx != null) {
+			if(iTestCtx.getSuite().getParameter(Context.TEST_CONFIG) != null) {
+				File suiteFile = new File(iTestCtx.getSuite().getXmlSuite().getFileName());
+				String configFile = suiteFile.getPath().replace(suiteFile.getName(), "") + iTestCtx.getSuite().getParameter("testConfig");
+				NodeList nList;// =XMLHelper
+				Map<String, String> parameters = iTestCtx.getSuite().getXmlSuite().getParameters();
+			
+				for(int i = 0 ;i< nList.getLength() ; i++) {
+					Node nNode = nList.item(i);
+					parameters.put(nNode.getAttributes().getNamedItem("name").getNodeValue(), nNode.getAttributes().getNamedItem("value").getNodeValue());
+				}
+			}
+		}
+		return iTestCtx;
+	}
+	
+}
